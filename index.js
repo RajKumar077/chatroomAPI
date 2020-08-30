de5682ed7c0251d365d76adfdeb7856f98a64210
RajKumar077 <38110476+RajKumar077@users.noreply.github.com>
2020-08-30 13:16:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-var cors = require("cors");
+const io = require("socket.io")(http, { origins: "*:*" });
+//var cors = require("cors");
 
-app.use(cors());
+//app.use(cors());
 io.origins("*:*");
 io.on("connection", (socket) => {
   socket.on("message", ({ username, message }) => {
