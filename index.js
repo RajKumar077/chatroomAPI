9706713935b25e2b9bfe6576f74bc0217388860f
RajKumar077 <38110476+RajKumar077@users.noreply.github.com>
2020-08-30 13:36:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http, {
   handlePreflightRequest: (req, res) => {
+    console.log("handlePreflightRequest called");
     const headers = {
-      "Access-Control-Allow-Headers": "Content-Type, Authorization",
+      "Access-Control-Allow-Headerst": "Content-Type, Authorization",
       "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
       "Access-Control-Allow-Credentials": true,
     };
