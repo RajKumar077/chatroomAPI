88326899ad764a79b32ebb6806cccca61cf08236
RajKumar077 <38110476+RajKumar077@users.noreply.github.com>
2020-08-30 13:40:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const app = require("express")();
 const http = require("http").createServer(app);
-const io = require("socket.io")(http, {
-  handlePreflightRequest: (req, res) => {
-    console.log("handlePreflightRequest called");
-    const headers = {
-      "Access-Control-Allow-Headerst": "Content-Type, Authorization",
-      "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
-      "Access-Control-Allow-Credentials": true,
-    };
-    res.writeHead(200, headers);
-    res.end();
-  },
-});
+const io = require("socket.io")(http, { origins: "*:*" });
 
 io.on("connection", (socket) => {
   socket.on("message", ({ username, message }) => {
@@ -19,6 +8,8 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(8080, function () {
+const port = process.env.PORT || 8080;
+
+http.listen(port, function () {
   console.log("listening on 8080..");
 });
